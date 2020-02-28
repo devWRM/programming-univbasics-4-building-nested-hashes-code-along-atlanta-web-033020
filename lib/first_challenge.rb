47a849293618a51d64e427e32ff171959424b4f3
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-02-28 15:34:58 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,8 +1,18 @@
 def first_challenge
    epic_tragedy = {
-      :montague => {},
+     :montague => {
+         :patriarch => {},
+         :matriarch => {},
+         :hero => {},
+         :hero_friends => []
+       },
+     :capulet => {
+         :patriarch => {},
+         :matriarch => {},
+         :heroine => {},
+         :heroine_friends => []
+       }
 
-      :capulet => {}
 
    }
 end
